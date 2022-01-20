46f2ef11765b75a5acb985dab721c3afba065a41
Leonardo Saldaña <leonardo.soldona@avalith.net>
2022-01-20 15:43:37 -0500

    leer datos desde s3

--- users-backup-aws-lambda/internal/processor/processor.go
+++ users-backup-aws-lambda/internal/processor/processor.go
@@ -2,6 +2,8 @@ package processor
 
 import (
 	"context"
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
 	"users-backup-aws-lambda/pkg/dto"
 )
 
@@ -13,7 +15,11 @@ func NewProcessorDummy() *processorDummy {
 	return &processorDummy{}
 }
 
-func (p *processorDummy) Process(ctx context.Context) (dto.Output, error) {
+func (p *processorDummy) Process(ctx context.Context, s3Event events.S3Event) (dto.Output, error) {
+	for _, record := range s3Event.Records {
+		s3 := record.S3
+		fmt.Printf("[%s - %s] Bucket = %s, Key = %s \n", record.EventSource, record.EventTime, s3.Bucket.Name, s3.Object.Key)
+	}
 	return dto.Output{
 		Mensaje: "Hola mundo",
 	}, nil
--- users-backup-aws-lambda/pkg/handler/handler.go
+++ users-backup-aws-lambda/pkg/handler/handler.go
@@ -2,13 +2,14 @@ package handler
 
 import (
 	"context"
+	"github.com/aws/aws-lambda-go/events"
 
 	"users-backup-aws-lambda/pkg/dto"
 )
 
 type (
 	processor interface {
-		Process(context.Context) (dto.Output, error)
+		Process(context.Context, events.S3Event) (dto.Output, error)
 	}
 
 	LambdaHandler struct {
@@ -20,7 +21,7 @@ func NewLambdaHandler(p processor) *LambdaHandler {
 	return &LambdaHandler{processor: p}
 }
 
-func (h *LambdaHandler) HandleRequest(ctx context.Context) (dto.Output, error) {
+func (h *LambdaHandler) HandleRequest(ctx context.Context, s3Event events.S3Event) (dto.Output, error) {
 
-	return h.processor.Process(ctx)
+	return h.processor.Process(ctx, s3Event)
 }
